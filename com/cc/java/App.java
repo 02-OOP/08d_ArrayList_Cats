c448f99fd1418a5aaf7b56a3e3d1e3bce8253443
Peter <schmidt.multimedia@gmail.com>
2022-04-27 13:04:04 +0200

    objList / Container für Referenzen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,7 @@
 package com.cc.java;
 
+import java.util.ArrayList;
+
 public class App {
     
     public static void main(String[] args) {
@@ -15,11 +17,15 @@ public class App {
        //cat1 / cat2 .. more of the same ...DRY
 
         String [] nameArr = {"Grizabella","Rum Tum Tugger","Alonzo","Suzanne"};
+        ArrayList<Cat> objList = new ArrayList<>();
 
         for (int i = 0; i < nameArr.length; i++) {
-            new Cat(nameArr[i]); // Objekte , ABER keine Referenz
+            // new Cat(nameArr[i]); // Objekte , ABER keine Referenz
+            objList.add(new Cat(nameArr[i]));
         }
 
+        output("Cats: " + objList);
+        output(objList.get(0).getAttributes());
 
 
     }
